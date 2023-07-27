03e88f244cb7c803b00d91cc6b28de7e02f77c7d
HardikShah563 <hardikts@gmail.com>
2023-07-27 19:11:42 +0530

    creating store layout

---
+++ static/script.js
@@ -0,0 +1,156 @@
+products = [{
+    name: "SACK OF ONION",
+    qty: "1kg",
+    price: 54.99,
+    type: "loose",
+    url: "onions"
+}, {
+    name: "FRESH CARROTS",
+    qty: "500g",
+    price: 34.49,
+    type: "loose",
+    url: "carrots"
+}, {
+    name: "SACK OF POTATOS",
+    qty: "1kg",
+    price: 59.99,
+    type: "loose",
+    url: "potatos"
+}, {
+    name: "LADY FINGER",
+    qty: "500g",
+    price: 54.99,
+    type: "loose",
+    url: "ladyfinger"
+}, {
+    name: "TOMATOS",
+    qty: "250g",
+    price: 129.99,
+    type: "loose",
+    url: "tomatos"
+}, {
+    name: "CORN",
+    qty: "1 full",
+    price: 18.49,
+    type: "loose",
+    url: "corn"
+}, {
+    name: "BANANAS",
+    qty: "1/2 Dozen",
+    price: 54.99,
+    type: "loose",
+    url: "bananas"
+}, {
+    name: "JUICY LEMONS",
+    qty: "4 pieces",
+    price: 11.99,
+    type: "loose",
+    url: "lemon"
+}, {
+    name: "BUTTER",
+    qty: "",
+    price: 54.99,
+    type: "packaged",
+    url: "butter"
+}, {
+    name: "SLICED BREAD",
+    qty: "",
+    price: 54.99,
+    type: "packaged",
+    url: "bread"
+}, {
+    name: "BUN",
+    qty: "6 pieces",
+    price: 24.99,
+    type: "packaged",
+    url: "bun"
+}, {
+    name: "MILK",
+    qty: "500ml",
+    price: 54.49,
+    type: "packaged",
+    url: "milk"
+}, {
+    name: "FROZEN GREEN PEAS",
+    qty: "500g",
+    price: 129.99,
+    type: "packaged",
+    url: "peas"
+}, {
+    name: "EGGS",
+    qty: "6 pieces",
+    price: 59.99,
+    type: "packaged",
+    url: "eggs6"
+}, {
+    name: "EGGS",
+    qty: "8 pieces",
+    price: 79.99,
+    type: "packaged",
+    url: "eggs8"
+}, {
+    name: "PASTA",
+    qty: "100g",
+    price: 24.99,
+    type: "packaged",
+    url: "pasta"
+}, {
+    name: "CHEESE SPREAD",
+    qty: "250g",
+    price: 144.99,
+    type: "packaged",
+    url: "cheese"
+}, {
+    name: "CHOCOLATE BAR",
+    qty: "100g",
+    price: 24.99,
+    type: "packaged",
+    url: "chocolate"
+}, {
+    name: "CEREALS",
+    qty: "250g",
+    price: 63.49,
+    type: "packaged",
+    url: "cereals"
+}];
+
+let emptyDiv = document.getElementById("store1");
+
+function storeItem() {
+    for (let i = products.length - 1; i >= 0; i--) {
+        if (products[i].type === "loose") {
+            let newProductItem = `
+                <div class="store-item">
+                    <div class="flex">
+                        <div class="store-item-img">
+                            <img src="./assets/store-banner.png" alt="store-item">
+                        </div>
+                        
+                        <div class="store-item-text">
+                            <p class="store-item-title">
+                            ${products[i].name} (${products[i].qty})
+                            </p>
+                            <p class="store-item-price">
+                                ₹79.99
+                            </p>
+                        </div>
+                    </div>
+            
+                    <div class="quantity">
+                        <div class="minus">
+                            <i class="fa-solid fa-minus"></i>
+                        </div>
+                        <div class="qty">01</div>
+                        <div class="plus">
+                            <i class="fa-solid fa-plus"></i>
+                        </div>
+                    </div>
+                </div>
+            `;
+            emptyDiv.insertAdjacentHTML("afterbegin", newProductItem);
+        }
+        console.log("This works!");
+    }
+}
+
+storeItem();
